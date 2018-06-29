1c3320b3dd6769244f85a691e11ae1ee5a183767
Shay <38899933+thirteenyahs@users.noreply.github.com>
2018-06-28 20:12:28 -0700

    Codecademy generated from the project submission page.

--- TomeRater/TomeRater.py
+++ TomeRater/TomeRater.py
@@ -3,13 +3,13 @@ class User(object):
 		self.name = name
 		self.email = email
 		self.books = {}
-
-	def get_email(self):
+		
+	def get_mail(self):
 		return self.email
-
+	
 	def change_email(self, address):
 		self.email = address
-		print (self.name + " has updated their email address to " + self.email)
+		print(self.name + " has updated their email address to " + self.email)
 		
 	def get_average_rating(self):
 		average = 0
@@ -18,20 +18,19 @@ class User(object):
 			if value:
 				average += value
 				books_rated += 1
-		average = average/books_rated		
+		average = average/books_rated
 		return average
-		
 		
 	def read_book(self, book, rating = None):
 		self.books[book] = rating
-
-	def __repr__(self):
-		return "User " + self.name + ", EMAIL ADDRESS: " + self.email + ", number of books read: " + str(len(self.books)
+		
+	def _repr_(self):
+		return "User " + self.name + ", EMAIL ADDRESS: " + self.email + ", number of books read: " + str(len(self.books))
 		
 	def __eq__(self, other_user):
 		if self.name == other_user.name and self.email == other_user.email:
 			return True
-        return False
+		return False
 		
 class Book(object):
 	def __init__(self, title, isbn):
@@ -47,13 +46,13 @@ class Book(object):
 	
 	def set_isbn(self, isbn):
 		self.isbn = isbn
-	print("The title: " + self.title + " now has a ISBN of "+ str(self.isbn))
+		print("The title: " + self.title + " now has a ISBN of "+ str(self.isbn))
 		
 	def add_rating(self, rating):
 		if rating > 0 and rating <= 4:
 			self.ratings.append(rating)
 		else:
-			print "Invalid Rating"
+			print ("Invalid Rating")
 
 	def __eq__(self, other_book):
 		if self.title == other_book.title and self.isbn == other_book.isbn:
@@ -70,10 +69,9 @@ class Book(object):
 	def	__hash__(self):
 		return hash((self.title, self.isbn))
 
-	
 class Fiction(Book):
 	def __init__(self, title, author, isbn):
-		Book.__init__(self, title, isbn):
+		Book.__init__(self, title, isbn)
 		self.author = author
 	
 	def get_author(self):
@@ -83,8 +81,8 @@ class Fiction(Book):
 		return self.title + " by " + self.author
 		
 class Non_Fiction(Book):
-	def __init__(self, title, subject, level, isbn)
-		Book.__init__(self, title, isbn):
+	def __init__(self, title, subject, level, isbn):
+		Book.__init__(self, title, isbn)
 		self.subject = subject
 		self.level = level
 	
@@ -116,16 +114,16 @@ class TomeRater(object):
 	
 	def add_book_to_user(self, book, email, rating = None):
 		user = self.users.get(email, None)
-			if user:
-				user.read_book(book, rating)
-				if book not in self.books:
-					self.books[book] = 0
-				self.books[book] +=1
-				book.add_rating(rating)
-			else:
-				print("No user with email " + email + "!")
+		if user:
+			user.read_book(book, rating)
+			if book not in self.books:
+				self.books[book] = 0
+			self.books[book] +=1
+			book.add_rating(rating)
+		else:
+			print("No user with email " + email + "!")
 				
-	def add_user(self, name, email, book_list = None)
+	def add_user(self, name, email, book_list = None):
 		new_user = User(name, email)
 		self.users[email] = new_user
 		if user_books:
@@ -141,28 +139,27 @@ class TomeRater(object):
 			print (users)
 			
 	def most_read_book(self):
-        max_reads = float("-inf")
-        most_read = None
-
-        for book in self.books:
-            read = self.books[book]
-            if read > max_reads:
-                max_reads = read
-                most_read = book
-
-        return most_read
+		max_reads = float("-inf")
+		most_read = None
+		
+		for book in self.books:
+			read = self.books[book]
+			if read > max_reads:
+				max_reads = read
+				most_read = book
+		return most_read
 		
 	def highest_rated_book(self):
 		highest_rating = float("-inf")
 		highest_rated_book = None
 
-        for book in self.books:
-            average = book.get_average_rating()
-            if average > highest_rating:
-                highest_rating = average
-                highest_rated_book = book
+		for book in self.books:
+			average = book.get_average_rating()
+			if average > highest_rating:
+				highest_rating = average
+				highest_rated_book = book
 
-        return highest_rated_book
+		return highest_rated_book
 		
 	def most_positive_user(self):
 		highest_rated = flat("-inf")
